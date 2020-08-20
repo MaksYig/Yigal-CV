dd591fa986461c39894a5ce1cf403d920465fd04
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2020-08-20 22:05:05 +0300

    added burger menu

--- app/js/main.js
+++ app/js/main.js
@@ -71,21 +71,24 @@ $(function () {
     /*MODIFICATION END*/
   });
 
-  $("#menu").on("click","a", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
+  $("#menu").on("click", "a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
 
-		//забираем идентификатор бока с атрибута href
-		var id  = $(this).attr('href'),
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr("href"),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
 
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top;
-		
-		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top}, 1500);
-	});
+    //анимируем переход на расстояние - top за 1500 мс
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
+
+  $(".header__burger").on("click", function () {
+    $(".header__burger").toggleClass("close");
+    $(".asside__bar").toggleClass("close");
+    
+  });
 
 
-
-  
 });
