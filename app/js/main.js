3ac34cca84e101177b6d8885c753876e21880771
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2020-08-27 22:10:55 +0300

    added portfolio and adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -105,17 +105,19 @@ $(function () {
     });
     /*MODIFICATION END*/
   });
-
-  $("#menu").on("click", "a", function (event) {
-    //отменяем стандартную обработку нажатия по ссылке
-    event.preventDefault();
-
-    //забираем идентификатор бока с атрибута href
-    var id = $(this).attr("href"),
-      //узнаем высоту от начала страницы до блока на который ссылается якорь
-      top = $(id).offset().top;
-
-    //анимируем переход на расстояние - top за 1500 мс
-    $("body,html").animate({ scrollTop: top }, 1500);
+  $(document).ready(function () {
+    //Плавно прокручивает страницу до id-ка
+    //Ссылка должна быть с id на якорь и классом prokrutkaslide.
+    $(".scroll").click(function () {
+      var el = $(this).attr("href");
+      el = el.replace(/[^\#]*/, ""); //вытаскиваем id из ссылки
+      $("body,html").animate(
+        {
+          scrollTop: $(el).offset().top,
+        },
+        2000
+      );
+      return false;
+    });
   });
 });
