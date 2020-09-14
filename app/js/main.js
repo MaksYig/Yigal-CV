04899841e68bfe475f1de22bbdf2dfbc0bbddc9e
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2020-09-14 20:57:02 +0300

    fixed little bugs

--- app/js/main.js
+++ app/js/main.js
@@ -212,20 +212,22 @@ $(function () {
 
   // var mixer = mixitup(".portfolio__items");
 
-  let offsetTop = $("#skills").offset().top;
-  $(window).scroll(function () {
-    var height = $(window).height();
-    if ($(window).scrollTop() + height > offsetTop) {
-      jQuery(".skillbar").each(function () {
-        jQuery(this)
-          .find(".skillbar-bar")
-          .animate(
-            {
-              width: jQuery(this).attr("data-percent"),
-            },
-            3000
-          );
-      });
-    }
-  });
+  try {
+    let offsetTop = $("#skills").offset().top;
+    $(window).scroll(function () {
+      var height = $(window).height();
+      if ($(window).scrollTop() + height > offsetTop) {
+        jQuery(".skillbar").each(function () {
+          jQuery(this)
+            .find(".skillbar-bar")
+            .animate(
+              {
+                width: jQuery(this).attr("data-percent"),
+              },
+              3000
+            );
+        });
+      }
+    });
+  } catch (e) {}
 });
